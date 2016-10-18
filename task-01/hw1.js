44f4460f62513d039903758913829a203dd581af
YanaPlotnyk <ploya@i.ua>
2016-10-18 22:04:09 +0300

    added the missing condition of the task

--- task-01/hw1.js
+++ task-01/hw1.js
@@ -1,13 +1,14 @@
 console.log("HomeWork1");
 
 function sequence(start, step){
+	if (isNaN(start)) start = 0;
 	if (isNaN(step)) step = 1;
 	start = start - step;
 	return function(){
 		return start = start + step;		
 	};
 };
-var gen = sequence(11);
+var gen = sequence();
 var gen2 = sequence(20,5);
 console.log(gen());
 console.log(gen());
