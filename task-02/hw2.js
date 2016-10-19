d9f0ea0516bb39e387d433eb566a4aef19befbff
YanaPlotnyk <ploya@i.ua>
2016-10-19 22:04:36 +0300

    added comments

--- task-02/hw2.js
+++ task-02/hw2.js
@@ -1,8 +1,14 @@
 console.log("HomeWork2");
-
+/**
+ *
+ * @param start first number of sequence
+ * @param step indicates the step  sequence size
+ * @returns {Function} function generator, which on every call
+ * gives number more on the step size, and so on to infinity
+ */
 function sequence(start, step){
-	if (isNaN(start)) start = 0;
-	if (isNaN(step)) step = 1;
+	if (isNaN(start)) start = 0; //the initial default value is 0
+	if (isNaN(step)) step = 1; //the default step is 1
 	start = start - step;
 	return function(){
 		return start = start + step;		
@@ -10,19 +16,19 @@ function sequence(start, step){
 };
 var gen = sequence(2,2);
 var gen2 = sequence(20,5);
-/*console.log(gen());
-console.log(gen());
-console.log(gen2());
-console.log(gen2());
-console.log(gen2());
-console.log(gen());*/
 
+/**
+ * function calls the generation specified number of times
+ * @param generator
+ * @param x numbers of calls
+ * @returns {Array} an array with the results of  calls
+ */
 function take(generator, x){
 	var arr = [];
 	for (var i = 0; i<x; i++){
 		arr[i] = generator();
 	}
-	return console.log(arr);
+	return arr;
 };
-take(gen, 5); //[2,4,6,8,10]
-take(gen2, 2); //[20,25]
+console.log(take(gen, 5)); //[2,4,6,8,10]
+console.log(take(gen2, 2)); //[20,25]
